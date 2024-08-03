075729c80aca3e740080bb0aec2f54fbb15329ca
mojtaba <mojtaba.mohamed187@gmail.com>
2024-08-04 02:26:21 +0300

    feat: implement new class that initiate new redis client
    a new class `RedisClient` to initiate a new redis client. the class contains for methods isAlive() to check client connectivity get() to retrive a key, set() to set a new key and value, and del() to delete a key

---
+++ utils/redis.js
@@ -0,0 +1,38 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (err) => {
+      console.log('Redis error:', err.message);
+    });
+    this.client.on('connect', () => {
+      console.log('Redis connected');
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    const getAsync = promisify(this.client.get).bind(this.client);
+    return getAsync(key);
+  }
+
+  async set(key, value) {
+    const setAsync = promisify(this.client.set).bind(this.client);
+    return setAsync(key, value);
+  }
+
+  async del(key) {
+    const delAsync = promisify(this.client.del).bind(this.client);
+    return delAsync(key);
+  }
+
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
