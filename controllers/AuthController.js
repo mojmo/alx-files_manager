73d1f3d0d50d8b305cd86f982a2646455917deaf
mojtaba <mojtaba.mohamed187@gmail.com>
2024-08-05 22:57:48 +0300

    modify header name

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -34,7 +34,7 @@ module.exports = {
   },
 
   getDisconnect: async (req, res) => {
-    const token = req.headers['x-token'];
+    const token = req.headers['X-Token'];
     if (!token) {
       return res.status(401).json({ error: 'Unauthorized' });
     }
