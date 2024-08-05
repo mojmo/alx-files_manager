2572d043b2e67062f25bc119258b85470409d82c
mojtaba <mojtaba.mohamed187@gmail.com>
2024-08-05 15:28:43 +0300

    feat: Create a new user In the file routes/index.js, add a new endpoint:
    POST /users => UsersController.postNew
    a new file UsersController.js that contains the new endpoint:
    POST /users should create a new user in DB:
    - To create a user, you must specify an email and a password
    - If the email is missing, return an error Missing email with a status code 400
    - If the password is missing, return an error Missing password with a status code 400
    - If the email already exists in DB, return an error Already exist with a status code 400
    - The endpoint is returning the new user with only the email and the id (auto generated by MongoDB) with a status code 201
    - The new user will be saved in the collection users:
    email: same as the value received
    password: SHA1 value of the value received

---
+++ controllers/UsersController.js
@@ -0,0 +1,39 @@
+const { hash } = require('crypto');
+const dbClient = require('../utils/db');
+
+module.exports = {
+  postNew: async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
+
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+
+    const usersCollection = dbClient.db.collection('users');
+
+    // Check if the email already exists
+    const existingUser = await usersCollection.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ error: 'Already exist' });
+    }
+
+    // Hash the password using SHA1
+    const hashedPassword = hash('sha1').update(password).digest('hex');
+
+    // Insert the new user into the database
+    const result = await usersCollection.insertOne({
+      email,
+      password: hashedPassword
+    });
+
+    // Return the new user with only the email and id
+    res.status(201).json({
+      id: result.insertedId,
+      email
+    });
+  }
+};
