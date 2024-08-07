aad428a4eb73d57665509e84ba5d04dfe6978597
mojtaba <mojtaba.mohamed187@gmail.com>
2024-08-07 19:07:57 +0300

    feat(routes): Add endpoint to retrieve file content
    - Added new route in  for retrieving file content:
      - GET /files/:id/data => FilesController.getFile
    
    feat(controllers): Implement method to get file content
    
    - Added  method in :
      - Retrieves the content of a file based on the ID, ensuring proper authentication and file type checks.
      - Returns appropriate status codes and responses for various error conditions (e.g., file not found, unauthorized access, folder content request).
      - Uses the  module to determine the MIME-type of the file and sets the appropriate Content-Type header.
    
    These changes enable users to retrieve the content of files via the new endpoint, ensuring secure access and proper handling of file types.

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -1,6 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
 import fs from 'fs';
 import path from 'path';
+import mime from 'mime-types';
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
@@ -194,5 +195,41 @@ module.exports = {
     await dbClient.db.collection('files').updateOne({ _id: dbClient.ObjectId(fileId) }, { $set: { isPublic: false } });
     const updatedFile = await dbClient.db.collection('files').findOne({ _id: dbClient.ObjectId(fileId) });
     return res.status(200).json(updatedFile);
+  },
+
+  getFile: async (req, res) => {
+    const fileId = req.params.id;
+    const token = req.headers['x-token'];
+
+    const file = await dbClient.db.collection('files').findOne({ _id: dbClient.ObjectId(fileId) });
+    if (!file) {
+      return res.status(404).json({ error: 'Not found' });
+    }
+
+    if (!file.isPublic) {
+      if (!token) {
+        return res.status(404).json({ error: 'Not found' });
+      }
+
+      const key = `auth_${token}`;
+      const userId = await redisClient.get(key);
+      if (!userId || userId !== file.userId.toString()) {
+        return res.status(404).json({ error: 'Not found' });
+      }
+    }
+
+    if (file.type === 'folder') {
+      return res.status(400).json({ error: 'A folder doesn\'t have content' });
+    }
+
+    if (!fs.existsSync(file.localPath)) {
+      return res.status(404).json({ error: 'Not found' });
+    }
+
+    const fileContent = fs.readFileSync(file.localPath);
+    const mimeType = mime.lookup(file.name) || 'application/octet-stream';
+
+    res.set('Content-Type', mimeType);
+    return res.send(fileContent);
   },
 };
